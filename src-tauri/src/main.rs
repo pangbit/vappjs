c97e53edde33452ecf09661d494bf050183aaeda
xbc <xubochen365@foxmail.com>
2024-02-23 14:39:36 +0800

    upd, tauri frame update to 2.0 beta

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,7 +10,6 @@ mod xX509;
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_window::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .invoke_handler(tauri::generate_handler![
